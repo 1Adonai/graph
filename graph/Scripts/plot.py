de428e7ae350bf640ed68eae37daef7a0ff1a121
1Adonai <prokonsul888@gmail.com>
2024-01-15 18:09:58 +0000

    lorenz attractor

--- graph/Scripts/plot.py
+++ graph/Scripts/plot.py
@@ -1,26 +1,50 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-fig, ax = plt.subplots()
 
 path = '/home/rumin/Documents/Code/Web/graph/War/numbers.txt'
 file = open(path,'r')
 num = file.read().splitlines()
-a = num[0]
-b = num[1]
-c = num[2]
+s1 = int(num[0])
+r1 = int(num[1])
+b1 = float(num[2])
 file.close()
 
 
-fruits = ['apple', 'blueberry', 'cherry']
-counts = [a, b, c]
-bar_labels = ['red', 'blue', '_red']
-bar_colors = ['tab:red', 'tab:blue', 'tab:red']
+def lorenz(xyz, *, s=s1, r=r1, b=b1):
+    x, y, z = xyz
+    x_dot = s*(y - x)
+    y_dot = r*x - y - x*z
+    z_dot = x*y - b*z
+    return np.array([x_dot, y_dot, z_dot])
 
-ax.bar(fruits, counts, label=bar_labels, color=bar_colors)
 
-ax.set_ylabel('fruit supply')
-ax.set_title('Fruit supply by kind and color')
-ax.legend(title='Fruit color')
+dt = 0.01
+num_steps = 10000
+
+xyzs = np.empty((num_steps + 1, 3))  # Need one more for the initial values
+xyzs[0] = (0., 1., 1.05)  # Set initial values
+
+path = '/home/rumin/Documents/Code/Web/graph/War/numbers.txt'
+file = open(path,'r')
+num = file.read().splitlines()
+s = int(num[0])
+r = int(num[1])
+b = float(num[2])
+file.close()
+
+
+for i in range(num_steps):
+    xyzs[i + 1] = xyzs[i] + lorenz(xyzs[i]) * dt
+
+# Plot
+ax = plt.figure().add_subplot(projection='3d')
+
+ax.plot(*xyzs.T, lw=0.5)
+ax.set_xlabel("X Axis")
+ax.set_ylabel("Y Axis")
+ax.set_zlabel("Z Axis")
+ax.set_title("Lorenz Attractor")
 
 # Сохранение графика с указанием пути
 plt.savefig('/home/rumin/Documents/Code/Web/graph/static/main/img/plot.png')
